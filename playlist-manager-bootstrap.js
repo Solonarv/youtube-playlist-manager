6219ecc365c45957eba5cb060646673ecd190021
Solonarv <nstamm@gmx.de>
2016-01-18 18:09:23 +0100

    Work on the bootstrapper

--- playlist-manager-bootstrap.js
+++ playlist-manager-bootstrap.js
@@ -2,19 +2,40 @@
 
 (function(){
     var manager_url = chrome.extension.getURL("playlist-manager.html");
-    console.log(manager_url);
     
-    xhr = new XMLHttpRequest();
-    xhr.open("GET", manager_url, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.write(xhr.responseText);
-        }
-    };
-    xhr.send();
+    // document.write("<html><head><title>Loading YT Playlist Manager</title></head><body><h1>Loading...</h1></body></html>");
+    // document.body.style.background = "#b7b7b7";
     
-    document.body.innerHTML = "<h1>Loading...</h1>";
-    document.body.style.background = "#b7b7b7";
-    
-    //window.location.href = manager_url;
+    $.get("https://youtube.com", function(youtube) {
+        // youtube = $(youtube);
+        // youtube = resetHomepage(youtube);
+        
+        document.write(youtube);
+        
+        $.get(manager_url, function(manager) {
+            $("#content").append(manager);
+        }, "html");
+    }, "html");
 })();
+
+function resetHomepage(youtube) {
+    // Nuke the banner ad
+    $("#header", youtube).remove();
+    
+    // Nuke the content, keep the <div /> because that's where the manager will go later
+    $("#content", youtube).empty();
+    
+    // Nuke the header thingy
+    $("#masthead-appbar-container", youtube).remove();
+    
+    // We are not actually on the home page, the guide should reflect that
+    $(".guide-item-selected", youtube).removeClass("guide-item-selected");
+    
+    // Alter the "playlists" link (which for some reason doesn't have an ID) to point here
+    $("a[data-name='g-personal']", youtube).attr("href", "https://youtube.com/playlists");
+    
+    // Unfocus the search box
+    $("#masthead-search-term", youtube).blur();
+    
+    return youtube;
+}
